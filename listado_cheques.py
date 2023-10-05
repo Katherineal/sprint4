79d4ebcbd0c2e50116057a0b4afe8ab8135d540f
Katherine Alzamora <141969148+Katherineal@users.noreply.github.com>
2023-10-05 11:54:11 -0300

    Add files via upload

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,69 +1,107 @@
 import csv
-import pandas as pd
+import sys
 
-notes=''
-with open('listado_cheques.csv',newline='') as f:
-    data=csv.reader(f,delimiter=',')
-    notes=list(data)
+# Leer datos desde el archivo CSV
+notas = []
+with open('listado_cheques.csv', 'r', newline='') as f:
+    datos = csv.DictReader(f, delimiter=',')
+    for fila in datos:
+        notas.append(fila)
 
-df = pd.read_csv('listado_cheques.csv', index_col='NroCheque' , delimiter=',')
-
-def motrar_en_columnas(notes):
-
-    #Nombres de las columnas
-    column_names=notes[0]
-
-    #Maxima cantidad de filas segun datos 
-    column_widths = [max(len(str(row[i])) for row in notes) for i in range(len(column_names))]
+# Funcion para mostrar el csv
+def mostrar_en_columnas(data):
+    if not data:
+        print("No hay datos para mostrar.")
+        return
     
-    #Imprime los campos en cada columna
-    for i in range(len(column_names)):
-        print('{:{}}'.format(column_names[i],column_widths[i]),'|',end=' ')
+    # Obtener los nombres de las columnas
+    nombres_columnas = data[0]
+
+    # Inicializar los anchos de las columnas con la longitud de los nombres de las columnas
+    anchos_columnas = {columna: len(columna) for columna in nombres_columnas}
+
+    # Calcular los anchos máximos de las columnas
+    for fila in data:
+        for columna, valor in fila.items():
+            anchos_columnas[columna] = max(anchos_columnas[columna], len(str(valor)))
+
+    # Imprimir los nombres de las columnas
+    for columna in nombres_columnas:
+        print('{:{width}}'.format(columna, width=anchos_columnas[columna]), '|', end=' ')
     print('')
 
-    #Imprime una linea horizontal que permite separar los nombres de datos
-    for width in column_widths:
-        print('-'*width,'|', end=' ')
+    # Imprimir una línea horizontal para separar los nombres de las columnas de los datos
+    for columna in nombres_columnas:
+        print('-' * anchos_columnas[columna], '|', end=' ')
     print('')
 
-    #Imprime los datos separandolos con un '|'
-    for i in range(1,len(notes)):
-        for j in range(len(column_names)):
-            print(f"{notes[i][j]:<{column_widths[j]}} |", end=' ')
+    # Imprimir las filas de datos
+    for fila in data:
+        for columna, valor in fila.items():
+            print('{:{width}}'.format(valor, width=anchos_columnas[columna]), '|', end=' ')
         print('')
 
-def salidaUsuario():
-    eleccion = input("Elige si quieres ver los registros en un csv o en la consola: ")
-    if eleccion == 'csv':
-        df.to_csv('listado_cheques_filtrado.csv', encoding='latin-1', sep=';')
-    elif eleccion == 'consola':
-        motrar_en_columnas(notes)
-    else: 
-        pass
-print(salidaUsuario())
-
-def estadoCheque():
-    tipo = input("Ingrese si quiere ver los cheques en estado PENDIENTE, APROBADO o RECHAZADO: ")
-    if tipo.upper() == 'PENDIENTE':
-        registros = df.loc[df['Estado'] ==  tipo.lower()]
-        return registros
-    elif tipo.upper() == 'APROBADO':
-        registros = df.loc[df['Estado'] ==  tipo.lower()]
-        return registros
-    elif tipo.upper() == 'RECHAZADO':
-        registros = df.loc[df['Estado'] ==  tipo.lower()]
-        return registros
-print(estadoCheque())
-
-def segunDni(dni):
-    booleanito = df['DNI'] == dni
-    if booleanito.any():
-        registros = [df.loc[df['DNI'] ==  dni]]
-        return registros
+# Filtrar cheques por estado
+def filtrar_por_estado(data, estado_filtrar=None):
+    if estado_filtrar is not None:
+        resultados_filtrados = [fila for fila in data if fila['Estado'] == estado_filtrar]
+        if resultados_filtrados:
+            mostrar_en_columnas(resultados_filtrados)
+        else:
+            mostrar_en_columnas(data)
     else:
-        return "El DNI no esta en la base de datos."
-elegirDni=int(input("Filtrar segun DNI:"))
-print(segunDni(elegirDni))
+        mostrar_en_columnas(data)
+
+# Filtrar cheques por DNI
+def segunDni(dni, data):
+    cliente = [fila for fila in data if fila['DNI'] == dni]
+    if cliente:
+        return cliente
+    else:
+        return "El DNI no está en la base de datos."
+
+# Funcion ver cheques en consola o csv
+def salidaUsuario():
+    while True:
+        eleccion = input("Elige si quieres ver los cheques en un csv o en la consola: ").lower()
+        if eleccion == 'csv':
+            with open('listado_cheques_filtrado.csv', 'w', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=notas[0].keys(), delimiter=';')
+                writer.writeheader()
+                writer.writerows(notas)
+            print("Se ha guardado el archivo 'listado_cheques_filtrado.csv'.")
+            break   
+        elif eleccion == 'consola':
+            mostrar_en_columnas(notas)
+            break  
+        else:
+            print("Opción no válida. Por favor, selecciona 'csv' o 'consola'.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python script.py <DNI>")
+        sys.exit(1)
+
+    elegirDni = sys.argv[1]
+    resultados = segunDni(elegirDni, notas)
+
+    if resultados == "El DNI no está en la base de datos.":
+        print(resultados)
+    else:
+        mostrar_en_columnas(resultados)
+        
+        # Preguntar por el estado de los cheques a mostrar
+        estado_filtrar = input("¿Qué tipo de cheques desea ver (APROBADO, RECHAZADO, PENDIENTE)? ").upper()
+
+        while estado_filtrar not in ['APROBADO', 'RECHAZADO', 'PENDIENTE']:
+            print("Estado no válido. Por favor, seleccione APROBADO, RECHAZADO o PENDIENTE.")
+            estado_filtrar = input("¿Qué tipo de cheques desea ver (APROBADO, RECHAZADO, PENDIENTE)? ").upper()
+            
+        if estado_filtrar in ['APROBADO', 'RECHAZADO', 'PENDIENTE']:
+            notas_filtradas = filtrar_por_estado(resultados, estado_filtrar)
+            salidaUsuario()  # Llama a la función para que el usuario elija la salida
+
+
 
 # def entreFechas():
 #     fecha_inicio_str = input("Ingrese la fecha de inicio en formato 'YYYY-MM-DD':")
@@ -92,17 +130,15 @@ print(segunDni(elegirDni))
 
 
 
-def verificarChequeRepetido():
-    dni = input("Ingrese el número de DNI: ")
-    numero_cheque = input("Ingrese el número de cheque: ")
+# def verificarChequeRepetido():
+#     dni = input("Ingrese el número de DNI: ")
+#     numero_cheque = input("Ingrese el número de cheque: ")
 
-    resultados = df[(df['DNI'] == dni) & (df['NumeroCheque'] == numero_cheque)]
+#     resultados = df[(df['DNI'] == dni) & (df['NumeroCheque'] == numero_cheque)]
 
-    if len(resultados) > 1:
-        print("¡Error! Se encontraron cheques repetidos en la misma cuenta para el DNI proporcionado.")
-    else:
-        print("No se encontraron cheques repetidos en la misma cuenta para el DNI proporcionado.")
+#     if len(resultados) > 1:
+#         print("¡Error! Se encontraron cheques repetidos en la misma cuenta para el DNI proporcionado.")
+#     else:
+#         print("No se encontraron cheques repetidos en la misma cuenta para el DNI proporcionado.")
 
-verificarChequeRepetido() 
-
-
+# verificarChequeRepetido() 
